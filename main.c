d0223fd33ec2ce968ef242b37a5e111d0ca754d5
PhonyFox <amirhossein.zb.a@gmail.com>
2024-12-27 13:04:23 +0330

    colors yes?

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 
 int main() {
     initscr();
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    attron(COLOR_PAIR(1));
-    printw("He d llo Racy! Thi    s  game will scare you :)");
-    attroff(COLOR_PAIR(1));
+    cbreak();
+    noecho();
+    printw("Hello World!");
+    int ch = getch();
+
+    printw("\nYou entered:");
+    printw("%c", ch);
     refresh();
     getch();
     endwin();
     return 0;
 }
+
